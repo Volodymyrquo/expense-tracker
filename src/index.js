d3a9b641c50187f12c6a59628004c8421e73081d
Volodymyr <volodymyrquo@gmail.com>
2020-12-25 13:46:59 +0200

    added SpeechProvider to index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Provider } from './context/context';
+import { SpeechProvider } from '@speechly/react-client';
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId='1835e0b2-6511-4847-b6d7-f1c4e9c62d21' language='en-US'>
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root')
 );
